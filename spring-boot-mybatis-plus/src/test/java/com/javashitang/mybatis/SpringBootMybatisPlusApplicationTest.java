327ca0982c680adce29a07a9ebcbefb456021d9c
li <lilm2@ziroom.com>
2024-03-18 12:42:57 +0800

    修改测试类

--- spring-boot-mybatis-plus/src/test/java/com/javashitang/mybatis/SpringBootMybatisPlusApplicationTest.java
+++ spring-boot-mybatis-plus/src/test/java/com/javashitang/mybatis/SpringBootMybatisPlusApplicationTest.java
@@ -1,14 +1,12 @@
 package com.javashitang.mybatis;
 
-import static org.junit.Assert.assertTrue;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.javashitang.mybatis.dao.BookDao;
 import com.javashitang.mybatis.entity.Book;
 import com.javashitang.mybatis.mapper.BookMapper;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -17,7 +15,6 @@ import javax.annotation.Resource;
 /**
  * Unit test for simple App.
  */
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootMybatisPlusApplicationTest {
 
--- spring-boot-sharding-jdbc/src/test/java/com/javashitang/sharding/SpringBootShardingApplicationTests.java
+++ spring-boot-sharding-jdbc/src/test/java/com/javashitang/sharding/SpringBootShardingApplicationTests.java
@@ -5,9 +5,7 @@ import com.javashitang.sharding.entity.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootShardingApplicationTests {
 
